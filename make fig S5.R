d3724cffdab78d9eaa680e552350d053937c97db
wfmorris <52839019+wfmorris@users.noreply.github.com>
2024-02-07 14:45:26 -0500

    Update make fig S5.R

--- make fig S5.R
+++ make fig S5.R
@@ -1,4 +1,4 @@
-# make panels of Fig S5 for Morris & Doak manuscript entitled 
+# make panels of Fig S5 for a manuscript entitled 
 # "What is adaptive demographic lability and when might we expect to see it?"
 
 graphics.off()
