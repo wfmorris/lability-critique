976693d22e57ace951dbba58d833c97e12afd9a7
wfmorris <52839019+wfmorris@users.noreply.github.com>
2024-02-07 14:44:48 -0500

    Update make fig 4.R

--- make fig 4.R
+++ make fig 4.R
@@ -1,4 +1,4 @@
-# make panels of Fig 4 for Morris & Doak manuscript entitled 
+# make panels of Fig 4 for a manuscript entitled 
 # "What is adaptive demographic lability and when might we expect to see it?"
 
 rm(list=ls(all=TRUE))
